7d050b70ab8c5e2df82beec7f0a373f903cb4ce2
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-02-15 12:26:50 +0530

    Updated app.component.html and angular.json, added Model files

---
+++ src/app/Model/Employee.ts
@@ -0,0 +1,22 @@
+export class Employee {
+    id: number;
+    name: string;
+    city: string;
+    state: string;
+    emailId: string;
+    contactNo: string;
+    address: string;
+    pinCode: string; 
+    
+    
+    constructor() {
+        this.id = 0;
+        this.name = '';
+        this.city = '';
+        this.state = '';
+        this.emailId = '';
+        this.contactNo = '';
+        this.address = '';
+        this.pinCode = '';
+    }
+}
